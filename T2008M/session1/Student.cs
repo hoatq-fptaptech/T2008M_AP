61e2d9ed9a89d5bdbdf6f23fa481d61a0c6ff249
billytrinh <quanghoa.trinh@gmail.com>
2021-05-17 10:57:30 +0700

    session 1

---
+++ T2008M/session1/Student.cs
@@ -0,0 +1,13 @@
+namespace T2008M.session1
+{
+    public class Student
+    {
+        public string name;
+        public static string sName;
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
