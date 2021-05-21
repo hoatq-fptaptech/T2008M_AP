41e0878ba3ccc4193e15a790083371c9d00cb5b2
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-05-21 09:31:28 +0700

    session 3

---
+++ T2008M_AP/session3/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace T2008M.session3
+{
+    public class Product
+    {
+        private string productName;
+        private decimal price;
+        public List<string> gallery = new List<string>(); 
+        // ban than List cung da co indexers
+        // indexers
+        public string this[int index]
+        {
+            get { return gallery[index]; }
+            set { gallery[index] = value; }
+        }
+        // Properties
+        public string ProductName
+        {
+            get => productName;
+            set => productName = value;
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price+10;
+            }
+            set
+            {
+                price = value-10;
+            }
+        }
+    }
+}
