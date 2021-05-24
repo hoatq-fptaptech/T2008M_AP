1be9c216fc6d8613702c4df8a560164b0da197ac
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-05-24 09:15:21 +0700

    demo delegate

---
+++ T2008M_AP/session4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T2008M.session4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+                x++;
+                int z = x / y;
+                Console.WriteLine("z = " + z);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Loi toan hoc");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally..");
+            }
+        }
+        // khi can lam viec voi I O (nhap -xuat)
+        // khi lam viec voi api (hoac connect ben ngoai qua internet)
+        // khi lam viec voi DB
+        // du doan se loi
+    }
+}
